b7712a21345afbf1d962ecf71084fa3afe6b9017
Alberto Marín <amarinme@gmail.com>
2025-02-04 13:00:08 +0100

    cambios en javascript para mejorar interfaz

--- local/ticketmanagement/amd/src/usermanagement/editUser.js
+++ local/ticketmanagement/amd/src/usermanagement/editUser.js
@@ -18,13 +18,20 @@ define([
     const showUserFormPopup= (e)=>{
         
         e.stopPropagation();
-        const userid=e.target.closest('.modify-user').dataset.userid;
+        const tr = e.target.closest('tr');
+        const userid=tr.dataset.userid;
+
+        // Accede al tercer <td> dentro del <tr>
+        const email = tr.querySelector('td:nth-child(3)').textContent;
+        const nombre= tr.querySelector('td:nth-child(4)').textContent;
+        const apellidos=tr.querySelector('td:nth-child(5)').textContent;
+
         const formpopup="UserFormPopup";
         
         const modalForm=new ModalForm({
             formClass: `\\local_ticketmanagement\\form\\${formpopup}`,
             args: {userid: userid},
-            modalConfig: {title: `User details: #${userid}`},
+            modalConfig: {title: `User details: ${nombre}, ${apellidos}`},
             returnFocus:e.target
         });
 
